9c5e193af3a83330db869653f713320912bae33b
Vinayak006 <vinayakamirtharaj28@gmail.com>
2025-02-14 10:30:54 +0530

    added launcher icon and splash icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import "package:flutter/material.dart";
+import "package:flutter_native_splash/flutter_native_splash.dart";
 
 import "up_todo.dart";
 
 void main() {
+  final WidgetsBinding widgetsBinding =
+      WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const UpTodo());
 }
